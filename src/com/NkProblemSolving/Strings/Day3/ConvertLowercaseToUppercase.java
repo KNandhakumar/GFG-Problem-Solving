75f1a3d0a601e69c233dac06008b119e4df295c9
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-04 07:38:07 +0530

    Convert lowercase to uppercase

---
+++ src/com/NkProblemSolving/Strings/Day3/ConvertLowercaseToUppercase.java
@@ -0,0 +1,11 @@
+package com.NkProblemSolving.Strings.Day3;
+
+import java.util.Scanner;
+
+public class ConvertLowercaseToUppercase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println(name.toUpperCase());
+    }
+}
