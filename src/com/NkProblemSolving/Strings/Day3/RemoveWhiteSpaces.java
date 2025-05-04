39c12ee15c7715374c798f11abdf2bf7b9d6deec
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-04 08:09:32 +0530

    Remove white spaces

---
+++ src/com/NkProblemSolving/Strings/Day3/RemoveWhiteSpaces.java
@@ -0,0 +1,8 @@
+package com.NkProblemSolving.Strings.Day3;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+        String name = "a b c d";
+        System.out.println(name.replaceAll(" ",""));
+    }
+}
