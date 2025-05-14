d9101d46dd00e8674d3a7e7ec16406a2191c99ce
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-14 05:38:30 +0530

    Pyramid Triangle Pattern            *           * *          * * *         * * * *        * * * * *

---
+++ src/com/NkProblemSolving/PatternPrinting/AdvancePatterns/PyramidStarts.java
@@ -0,0 +1,16 @@
+package com.NkProblemSolving.PatternPrinting.AdvancePatterns;
+
+public class PyramidStarts {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n ; i++) {
+            for (int space = 1; space <= n-i ; space++) {
+                System.out.print(" ");
+            }
+            for (int start = 1; start <= i ; start++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
